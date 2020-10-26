bd4c661e7cfae12a4b044f4130dc1079cefd7bc7
unknown <76663452163.com>
2020-10-26 17:32:57 +0800

    小程序初体验三：事件监听

--- pages/home/home.js
+++ pages/home/home.js
@@ -6,7 +6,19 @@ Page({
    */
   data: {
 
+    counter:0
+
   },
+
+    // 按钮点击增加计数方法
+    onClickPlus(){
+      //console.log('+号被点击了')
+      //不能直接使用this.data.counter=xx的方法改变计数器，小程序没有vue的相关机制，不会自动渲染页面变化
+      //需要使用this.setData()方法,页面才会即时刷新
+      this.setData({
+        counter:this.data.counter + 1
+      })
+    },
 
   /**
    * 生命周期函数--监听页面加载
