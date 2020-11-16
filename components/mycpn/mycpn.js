9e2798de95c808ce9a78b3d68d1678726591f471
unknown <76663452163.com>
2020-11-16 17:55:52 +0800

    自定义组件

---
+++ components/mycpn/mycpn.js
@@ -0,0 +1,25 @@
+// components/mycpn.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    myCpnTitle:'我是自定义组件的标题',
+    myCpnContent:'自定义组件的内容'
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
---
+++ components/mystyle/mystyle.js
@@ -0,0 +1,8 @@
+// components/mystyle/mystyle.js
+Component({
+
+  options:{
+    styleIsolation:"shared" //默认是isolated：隔离； apply-shared:单向影响，页面样式影响组件样式；  shared：互相影响
+
+  }
+})
