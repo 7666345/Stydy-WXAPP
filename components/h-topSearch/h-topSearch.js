fc0423e64a853820edc2027b417519fe5c4df21c
7666345 <76663452163.com>
2020-11-27 14:19:43 +0800

    商城

---
+++ components/h-topSearch/h-topSearch.js
@@ -0,0 +1,28 @@
+// components/h-topSearch.js
+const app=getApp()
+
+
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    navH:app.globalData.navHeight
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
--- pages/home/home.js
+++ pages/home/home.js
@@ -1,10 +1,13 @@
-// pages/home/home.js
+//获取APP对象实例
+const app=getApp();
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    "homeItems":['../../assets/images/1.jpg','../../assets/images/1.jpg','../../assets/images/1.jpg']
 
   },
 
@@ -12,7 +15,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+      
   },
 
   /**
